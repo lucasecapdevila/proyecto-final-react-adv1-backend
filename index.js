0f153817d7b06b219aa7b7ce794df0c824e97afe
Lucas Capdevila <lcapdevila60@gmail.com>
2024-07-28 17:25:40 -0300

    conexión a la DB terminada

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import cors from 'cors'
 import morgan from 'morgan';
 import path from 'path'
 import { fileURLToPath } from 'url';
+import './src/database/db.js'
 
 
 const app = express()
 
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), () => {
-  console.log(`Estoy en el puerto ${app.get('port')}`);
+  console.log(`Puerto ${app.get('port')}`);
 })
 
 app.use(cors())
